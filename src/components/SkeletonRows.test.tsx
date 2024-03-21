ed6228cee60a5d6d3a6bf499b9047e27b1e9e49f
Alex Macdonald <almacdon@redhat.com>
2024-03-21 10:06:29 -0400

    unit tests for the loading card

--- src/components/SkeletonRows.test.tsx
+++ src/components/SkeletonRows.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SkeletonRows from './SkeletonRows';
 
-describe('SkeletonRows', () => {
+describe('Skeleton Rows', () => {
   it('should render as many skeleton rows as specified', () => {
     const numRows = 5;
     const { container } = render(<SkeletonRows numRows={numRows} />);
